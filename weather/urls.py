fe03c7131bcb1a659bd6e33ee1c12a179d7f7174
Dawinartor <46395323+Dawinartor@users.noreply.github.com>
2023-12-04 15:09:46 +0100

    Add rainning.css and rainning.html with content from source: https://codepen.io/_Sabine/pen/QzzBve

--- weather/urls.py
+++ weather/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
+    path('rain', views.RainView.as_view(), name='rain'),
 ]
--- weather/views.py
+++ weather/views.py
@@ -5,3 +5,6 @@ from django.views.generic import TemplateView
 # Create your views here.
 class HomeView(TemplateView):
     template_name = 'weather/home.html'
+
+class RainView(TemplateView):
+    template_name = 'weather/rainning.html'
