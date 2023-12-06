142ab0d28f1fcc46087fa7ea4e0fe8ef3c36672c
David Melamed <dmelamed@avodaq.com>
2023-12-06 16:33:19 +0100

    update

--- weather/views.py
+++ weather/views.py
@@ -1,6 +1,7 @@
+import os
+from geopy.geocoders import Nominatim
 from django.shortcuts import render
 from django.views.generic import TemplateView
-
 
 
 
@@ -10,6 +11,32 @@ def HomeView(request):
     current_weather = "Sun"
     context = {"current_weather": current_weather}
     return render(request, "weather/home.html", context)
+
+class HomeView(request):
+    api_key = os.environ("API_KEY")
+    current_weather = 0
+
+    def get_geolocation(self, city_name):
+        city = str(city_name)
+        geolocator = Nominatim(user_agent="geo_location_app")
+        location = geolocator.geocode(city)
+
+        # if location call was successfull extract longitude & latitude
+        if location:
+            lat, long = location.latitude, location.longitude
+            return lat, long
+        else:
+            print(f"Unable to get geolocation for {city}")
+            return None
+
+
+
+    def get_weather(self):
+        params = {"lat":}
+        base_url =  f"https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}"
+
+
+    #def change_component(self):
 
 class RainView(TemplateView):
     template_name = 'weather/rainning.html'
