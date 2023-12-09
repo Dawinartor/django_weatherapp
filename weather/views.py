c6a71acf16de0e28a83d985858ad7f492f08d8c3
melamed <melameddavid19@gmail.com>
2023-12-09 22:22:10 +0100

    Sucessfully implemented API call and context['current_weather_status'] based template_rendering

--- weather/views.py
+++ weather/views.py
@@ -4,27 +4,39 @@ from .utilities import WeatherCaller
 
 
 # Create your views here.
-
-weather_util = WeatherCaller()
-base_url = weather_util.get_url("Bremen", "") #TODO: country is optional
-print(base_url)
-weather_data = weather_util.get_weather_data_raw(base_url)
-print(weather_data)
-ret = weather_util.get_webther_format(weather_data)
-print(ret)
-
-def HomeView(request):
-    current_weather = "Sun"
-    context = {"current_weather": current_weather}
-    return render(request, "weather/home.html", context)
-
-def WebtherView(TemplateView): #todo: implement api call and showing data in view
-    template_name = 'base.html'
-
+def WebtherView(request):
     weather_templates = {
-        'rainny': 'weather/rain.html',
-        'sunny': 'weather/sun.html',
+        'rainny': 'weather/rainning.html',
+        'sunny': 'weather/sunny.html',
         'clear': 'weather/clear.html'
     }
+    template_name = ""
+
+    weather_util = WeatherCaller()
+    base_url = weather_util.get_url("Bremen", "Germany")
+    raw_data = weather_util.get_weather_data_raw(base_url)
+    context = weather_util.get_webther_format(raw_data)
+
+    # conditional template switch logic
+    if context['current_weather_status'] == "Drizzel" or "Rain" or "Thunderstorm" or "Snow":
+        template_name = weather_templates['rainny']
+    if context['current_weather_status'] == "Sun":
+        template_name = weather_templates['sunny']
+        print(template_name)
+    if context['current_weather_status'] == "Clear":
+        template_name = weather_templates['clear']
+
+
+
+    return render(request, template_name, context)
+
+#def WebtherView(TemplateView): #todo: implement api call and showing data in view
+ #   template_name = 'base.html'
+
+  #  weather_templates = {
+   #     'rainny': 'weather/rain.html',
+    #    'sunny': 'weather/sun.html',
+     #   'clear': 'weather/clear.html'
+   # }
 
 
